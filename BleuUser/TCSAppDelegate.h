431e5f9c4a06c6f495e18b499f71fe239232b2c9
Steve Brokaw <brokaw@twocanoes.com>
2013-10-28 10:49:07 -0500

    Added comments. Removing Xcode user settings.

--- BleuUser/TCSAppDelegate.h
+++ BleuUser/TCSAppDelegate.h
@@ -9,8 +9,6 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-@protocol UIApplicationDelegate;
-
 @interface TCSAppDelegate : UIResponder <UIApplicationDelegate, CLLocationManagerDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
